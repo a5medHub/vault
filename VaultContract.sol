0274eaeae18d1624a8f9649de1b50d174dd20458
A.Yassine <95106484+a5medHub@users.noreply.github.com>
2022-09-12 17:59:18 +0300

    Update VaultContract.sol
    create the contract with struct

--- VaultContract.sol
+++ VaultContract.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.15;
+
+contract VaultContract{
+    struct Vault{
+        address creator;
+        string name;
+        address[] users;
+        uint amount;
+
+    }
